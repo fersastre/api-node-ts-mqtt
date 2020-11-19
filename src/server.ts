ab25b4d5b95f4c2dd5ab3e04d54984a78e8e15fb
fersastre <fersastre23@gmail.com>
2020-11-19 15:09:56 -0300

    Suscribcion a topic MQTT abierta durante ejecucucion

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,27 @@
 import app from "./app";
+
 const PORT = process.env.PORT || 3000;
  
 app.listen(PORT, () => {
 console.log(`API REST corriendo en puerto ${PORT}`);
+})
+
+import { connect } from 'mqtt';
+// var mqtt = require('mqtt')
+var client  = connect('mqtt://test.mosquitto.org')
+ 
+client.on('connect', function () {
+    console.log('Mqtt connected.')
+    client.subscribe('nodejsfer', function (err) {
+        if (err) {
+            console.log('Ocurrio un error al suscribir.')
+        // client.publish('nodejsfer', 'Hello mqtt')
+    }
+  })
+})
+ 
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+//   client.end()
 })
